3531e0bde08bf9fa0add8c16794b2857bbae4f77
Accoie <159988369+Accoie@users.noreply.github.com>
2024-10-04 22:16:48 +0300

    made makeTree function and more...

--- AndOrTree.cpp
+++ AndOrTree.cpp
@@ -23,51 +23,85 @@ class LogicTree {
 private:
     Tree* root;
     const set<char> logicOperations = {'*', '+'};
-public:
-    LogicTree() : root(nullptr) {};
-
-    Tree* addElemToTree(char ch) {
-        Tree* NewElem = new Tree(ch);
-        return NewElem;
-    }
-
-    void makeLogicTree(vector<char> temp_vector) {
-        Tree* node = nullptr;
-        stack<Tree*> stackOfLogicOperations;
-        for (auto it = temp_vector.end(); it != temp_vector.begin();) {
-            bool success = true;
-            node = addElemToTree(*it);
-            if (logicOperations.find(*it) != logicOperations.end()) { // check is our char operation
-                node = stackOfLogicOperations.top();
-                if (root == nullptr) {
-                    root = node;
-                    node->level = 1;
-                } else if (!node->right_child) {
-
-                } else if (!node->left_child) {
-
-                } else {
-                    stackOfLogicOperations.pop();
-                    success = false;
-                }
-                // добавляем в дерево
-            } else { // block for operands
-                if(!stackOfLogicOperations.empty()){
-                    node = stackOfLogicOperations.top();
-                    if (node->right_child == nullptr) {
-                        
-                    } else if(node->left_child == nullptr){
-
-                    }
-                }
-                
+    int addElementToTree(Tree*& operation, char ch) {
+        if (logicOperations.find(ch) != logicOperations.end()) {
+            Tree* NewElement = new Tree(ch);
+            if (!root) {
+                operation = NewElement;
+                root = operation;
+                operation->level = 1;
             }
-            if (success) {
-                it--;
+            else if (!operation->right_child) {
+                operation->right_child = NewElement;
+                NewElement->fath = operation;
+                NewElement->level = ++operation->level;
+                operation = operation->right_child;
             }
+            else if (!operation->left_child) {
+                operation->left_child = NewElement;
+                NewElement->fath = operation;
+                NewElement->level = ++operation->level;
+                operation = operation->left_child;
+            }
+            return 1;
+        }
+        else if (operation) {
+            Tree* NewElement = new Tree(ch);
+            if (!operation->right_child) {
+                operation->right_child = NewElement;
+                NewElement->fath = operation;
+                NewElement->level = 0;
+            }
+            else if (!operation->left_child) {
+                operation->left_child = NewElement;
+                NewElement->fath = operation;
+                NewElement->level = 0;
+                return 0;
+            }
+            return 2;
+        } else {
+            return 3;
         }
     }
+public:
+    LogicTree() : root(nullptr) {}; 
 
+    int makeLogicTree(vector<char> temp_vector) {
+        stack<Tree*> stackOfLogicOperations;
+        Tree* operation = nullptr;
+        int size_tree = 0;
+        for (auto it = temp_vector.end(); it != temp_vector.begin();) {
+            --it;
+            if (!stackOfLogicOperations.empty()) {
+                operation = stackOfLogicOperations.top();
+            } else if (root){
+                return 1;       
+            }
+            int push_pop_operation = addElementToTree(operation, *it);
+            if (push_pop_operation == 1) {
+                size_tree++;
+                stackOfLogicOperations.push(operation);
+            } else if (push_pop_operation == 0) {
+                stackOfLogicOperations.pop();
+            } else if (push_pop_operation == 3) {
+                return 1;
+            }
+        }
+        root->level = size_tree;
+        return 0;
+    }
+    void drawBranches(int shift) {
+        for (int i) {
+
+        }
+    }
+    void printLogicTree() {
+        int num_spaces = root->level * root->level;
+        string spaces(num_spaces, ' ');
+        cout << spaces << root->data;
+        drawBranches(root->level);
+
+    }
 };
 
 bool leaveProgram() {
@@ -98,30 +132,34 @@ int main() {
                 logicalExpression.push_back(ch);
             }
             inFile.close();
-            logicTree.makeLogicTree(logicalExpression);
-            
+            int result = logicTree.makeLogicTree(logicalExpression);
+            if (result == 0) {
+                cout << "Дерево заполнилось полностью. \n";
+            } else {
+                cout << "Не все символы вошли в структуру дерева: проверьте входные данные на наличие ошибок. \n";
+            } 
+            logicTree.printLogicTree();
         }  
         if (leaveProgram()) {
             return 0;
         }
     }
-    return 0;
 }
 /*if (result == root && result != nullptr) {
-                result->right_child = addElemToTree(*it);
+                result->right_child = addElementToTree(*it);
                 if (logicOperations.find(*it) != logicOperations.end()) { // проверяем является зн-е итератора операцией, поиском в сете
                     result = result->right_child;
                 }
             } else if(root && result->right_child == nullptr) {
-                result->right_child == addElemToTree(*it);
+                result->right_child == addElementToTree(*it);
             }
             else if(result->right_child != nullptr) {
-                result->left_child == addElemToTree(*it);
+                result->left_child == addElementToTree(*it);
                 if (logicOperations.find(*it) != logicOperations.end()) { // проверяем является зн-е итератора операцией, поиском в сете
                     result = result->left_child;
                 }
             }
             else if (root == nullptr) {
-                root, result = addElemToTree(*it);
+                root, result = addElementToTree(*it);
             }
             */
